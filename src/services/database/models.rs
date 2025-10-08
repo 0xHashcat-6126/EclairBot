a46dba427726193bd49d92d9ce10fcf8a9f9cecd
0xHashcat-6126 <hashcat6126@gmail.com>
2025-10-08 23:19:43 +0200

    Database models.rs update

--- src/services/database/models.rs
+++ src/services/database/models.rs
@@ -1,4 +1,5 @@
-use sqlx::FromRow;
+use sqlx::{FromRow, SqlitePool};
+use std::error::Error;
 
 #[derive(FromRow)]
 pub struct Member {
@@ -9,6 +10,35 @@ pub struct Member {
     bank: i32,
 }
 
+impl Member {
+    pub fn new(id: i32) -> Self {
+        Self {
+            id,
+            exp: 0,
+            reputation: 0,
+            cash: 0,
+            bank: 0,
+        }
+    }
+
+    pub async fn insert(&self, pool: &SqlitePool) -> Result<(), Box<dyn Error>> {
+        sqlx::query(
+            "INSERT INTO members (id, exp, reputation, cash, bank)
+            VALUES (?, ?, ?, ?, ?)
+            ON CONFLICT(id) DO NOTHING",
+        )
+        .bind(self.id)
+        .bind(self.exp)
+        .bind(self.reputation)
+        .bind(self.cash)
+        .bind(self.bank)
+        .execute(pool)
+        .await?;
+
+        Ok(())
+    }
+}
+
 #[derive(FromRow)]
 pub struct Economy {
     member_id: i32,
@@ -16,6 +46,35 @@ pub struct Economy {
     last_rob: i32,
     last_slut: i32,
     last_work: i32,
+}
+
+impl Economy {
+    pub fn new(member_id: i32) -> Self {
+        Self {
+            member_id,
+            last_crime: 0,
+            last_rob: 0,
+            last_slut: 0,
+            last_work: 0,
+        }
+    }
+
+    pub async fn insert(&self, pool: &SqlitePool) -> Result<(), Box<dyn Error>> {
+        sqlx::query(
+            "INSERT INTO economy (member_id, last_crime, last_rob, last_slut, last_work)
+            VALUES (?, ?, ?, ?, ?)
+            ON CONFLICT(id) DO NOTHING",
+        )
+        .bind(self.member_id)
+        .bind(self.last_crime)
+        .bind(self.last_rob)
+        .bind(self.last_slut)
+        .bind(self.last_work)
+        .execute(pool)
+        .await?;
+
+        Ok(())
+    }
 }
 
 #[derive(FromRow)]

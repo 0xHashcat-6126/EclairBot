c4adce7294533b2e075082dbb8acd284d8e178ce
0xHashcat-6126 <hashcat6126@gmail.com>
2025-10-12 01:19:41 +0200

    Optimization

--- src/utils/ignore_case_cmp.rs
+++ src/utils/ignore_case_cmp.rs
@@ -6,28 +6,38 @@ pub trait IgnoreCaseCmp {
 }
 
 impl IgnoreCaseCmp for &str {
-    fn ignore_case_eq(&self, other: &str) -> bool {
-        let mut self_chars = self.chars();
-        let mut other_chars = other.chars();
+    #[inline(always)]
+    fn ignore_case_eq(&self, b_string: &str) -> bool {
+        if self.as_ref().len() != b_string.len() {
+            return false;
+        }
 
-        while let (Some(a), Some(b)) = (self_chars.next(), other_chars.next()) {
-            if a.is_ascii() && b.is_ascii() {
-                if a.to_ascii_lowercase() != b.to_ascii_lowercase() {
-                    return false;
-                }
-            } else {
-                if a != b {
-                    return false;
+        let a_bytes = self.as_ref().as_bytes();
+        let b_bytes = b_string.as_bytes();
+
+        let mut i = 0;
+        while i + 8 <= a_bytes.len() {
+            let a_chunk = u64::from_le_bytes(a_bytes[i..i + 8].try_into().unwrap());
+            let b_chunk = u64::from_le_bytes(b_bytes[i..i + 8].try_into().unwrap());
+            if a_chunk != b_chunk {
+                for j in 0..8 {
+                    let a_char = a_bytes[i + j] | 0x20;
+                    let b_char = b_bytes[i + j] | 0x20;
+                    if a_char != b_char {
+                        return false;
+                    }
                 }
             }
+            i += 8;
         }
-        
-        match (self_chars.next(), other_chars.next()) {
-            (None, None) => true,
-            (Some(_), None) => false,
-            (None, Some(_)) => false,
-            (Some(_), Some(_)) => unreachable!(),
+
+        for k in i..a_bytes.len() {
+            if (a_bytes[k] | 0x20) != (b_bytes[k] | 0x20) {
+                return false;
+            }
         }
+
+        true
     }
 
     fn ignore_case_starts_with(&self, prefix: &str) -> bool {

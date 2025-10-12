90efded9bf3e2d87e3f5f39d0a82676cb2c956d1
0xHashcat-6126 <hashcat6126@gmail.com>
2025-10-12 14:08:08 +0200

    Optimization

--- src/utils/ignore_case_cmp.rs
+++ src/utils/ignore_case_cmp.rs
@@ -1,4 +1,5 @@
 pub trait IgnoreCaseCmp {
+    fn de_pl(string: &str) -> String;
     fn ignore_case_eq(&self, other: &str) -> bool;
     fn ignore_case_starts_with(&self, prefix: &str) -> bool;
     fn ignore_case_ends_with(&self, suffix: &str) -> bool;
@@ -6,6 +7,62 @@ pub trait IgnoreCaseCmp {
 }
 
 impl<T: AsRef<str>> IgnoreCaseCmp for T {
+    #[inline(always)]
+    fn de_pl(s: &str) -> String {
+        let mut out = String::with_capacity(s.len());
+        let bytes = s.as_bytes();
+        let mut i = 0;
+
+        while i < bytes.len() {
+            let b = bytes[i];
+
+            if b < 0x80 {
+                out.push(b as char);
+                i += 1;
+                continue;
+            }
+
+            if i + 1 >= bytes.len() {
+                out.push(b as char);
+                break;
+            }
+
+            let b2 = bytes[i + 1];
+
+            match (b, b2) {
+                (0xC4, 0x85) => out.push('a'),
+                (0xC4, 0x87) => out.push('c'),
+                (0xC4, 0x99) => out.push('e'),
+                (0xC5, 0x82) => out.push('l'),
+                (0xC5, 0x84) => out.push('n'),
+                (0xC3, 0xB3) => out.push('o'),
+                (0xC5, 0x9B) => out.push('s'),
+                (0xC5, 0xBA) => out.push('z'),
+                (0xC5, 0xBC) => out.push('z'),
+
+                (0xC4, 0x84) => out.push('A'),
+                (0xC4, 0x86) => out.push('C'),
+                (0xC4, 0x98) => out.push('E'),
+                (0xC5, 0x81) => out.push('L'),
+                (0xC5, 0x83) => out.push('N'),
+                (0xC3, 0x93) => out.push('O'),
+                (0xC5, 0x9A) => out.push('S'),
+                (0xC5, 0xB9) => out.push('Z'),
+                (0xC5, 0xBB) => out.push('Z'),
+
+                _ => {
+                    out.push(b as char);
+                    i += 1;
+                    continue;
+                }
+            }
+
+            i += 2;
+        }
+
+        out
+    }
+
     #[inline(always)]
     fn ignore_case_eq(&self, b_string: &str) -> bool {
         if self.as_ref().len() != b_string.len() {
